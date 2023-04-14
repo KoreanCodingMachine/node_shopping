cd5d2c5077d61e6c36d6b3a30e8f6f25a0e3e088
kimjuhyeong <pon03078@naver.com>
2023-04-14 17:14:52 +0900

    add get

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express  from 'express'
 const app = express()
 
+
+//requset response test
+app.get("/", (req, res) => {
+    res.json({
+        msg: 'get data'
+    })
+})
+
+
+
+
+
 const port = 8080
 app.listen(port, () => {
     console.log('server started')
