04fb6e357d728ef410a0ab3d3eb03d10d1a6a5b2
kimjuhyeong <pon03078@naver.com>
2023-04-14 19:29:26 +0900

    add delete get , detail delete , post detail post

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ const router = express.Router()
 // CRUD
 
 
-// Product 불러오기
+// 전체 Product 불러오기
 router.get('/', async (req, res) => {
     try {
         const products = await productModel.find()
@@ -19,6 +19,26 @@ router.get('/', async (req, res) => {
 
     }
 })
+
+// 상세 Product를 가져오기
+router.get('/:productId', async (req, res) => {
+    const { productId } = req.params
+    try {
+        const product = await productModel.findById(productId)
+        if (!product) {
+            res.json({
+                msg : 'no product'
+            })
+        }
+        res.json({
+            msg : 'successful get product',
+            product
+        })
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 
 // product 등록하기
 
@@ -62,18 +82,31 @@ router.put('/', async (req, res) => {
     }
 })
 
-// product 삭제하기
+// 상세 product 삭제하기
 
-router.delete('/', async (req, res) => {
+router.delete('/:productId', async (req, res) => {
+    const { productId } = req.params
     try {
+        await productModel.findByIdAndDelete(productId)
         res.json({
-            msg:'delete product'
+            msg:`deleted product at ${productId}`
         })
     } catch (err) {
 
     }
 })
 
+// 전체 product 삭제하기
+router.delete('/', async (req, res) => {
+    try {
+        await productModel.deleteMany()
+        res.json({
+            msg : 'deleted all products'
+        })
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 
 export default router
